c8da138509d2ac3c00a54973b9ca38a0649b95a1
lx521603 <x@glee.ch>
2025-11-24 01:44:06 +0800

    update

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,22 +1,7 @@
-export function generateSlug(string) {
-	return string
-		.toString()
-		.trim()
-		.toLowerCase()
-		.replace(/\s+/g, '-')
-		.replace(/[^\w\-]+/g, '')
-		.replace(/\-\-+/g, '-')
-		.replace(/^-+/, '')
-		.replace(/-+$/, '');
-}
-
-export function generateTagData(categories) {
-	let categoryData = [];
-	categories.forEach(category => {
-		categoryData.push({
-			title: category,
-			slug: `${generateSlug(category)}`,
-		});
-	});
-	return categoryData;
+export function generateTagData(tagsSet) {
+  const tagsArray = Array.from(tagsSet);
+  return tagsArray.filter(tag => tag && tag.trim() !== '').map(tag => ({
+    title: tag,
+    slug: tag.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
+  }));
 }
