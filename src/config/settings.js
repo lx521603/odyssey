a2aa959e4c9c213eadb619a5945acf9c174af743
lx521603 <x@glee.ch>
2025-11-24 01:50:45 +0800

    fix

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `Rich.yt`,
 	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `Rich`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: false,
+	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
